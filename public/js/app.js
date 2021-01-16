dd182c1a42658919b0c3ca343488387e58c25643
ritik <ritikjain1002@gmail.com>
2021-01-16 16:16:10 +0530

    Setup for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,7 @@ const para = document.querySelector("p");
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const location = search.value;
-  fetch("http://localhost:3000/weather?location=" + location)
+  fetch("/weather?location=" + location)
     .then((response) => response.json())
     .then((json) => {
       para.innerHTML = JSON.stringify(json);
--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Path for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -78,6 +79,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server is up and running on port 3000");
 });
